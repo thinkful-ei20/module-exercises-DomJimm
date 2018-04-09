9d9ee84906c5717bda1fec99d03c385321df6cac
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-09 11:56:09 -0400

    Added local variables to namespace

--- scripts/store.js
+++ scripts/store.js
@@ -1,16 +1,17 @@
 'use strict';
 /*global cuid*/
 //eslint-disable-next-line no-unused-vars
-const store = (function (){
-  return {
-    items: [
-      { id: cuid(), name: 'apples', checked: false },
-      { id: cuid(), name: 'oranges', checked: false },
-      { id: cuid(), name: 'milk', checked: true },
-      { id: cuid(), name: 'bread', checked: false }
-    ],
-    hideCheckedItems: false,
-    searchTerm: '',
-  };
 
+const store = (function (){
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+  return {
+    items, hideCheckedItems, searchTerm
+  };
 }());
