c97e1311f70737fa23adb6e7b0198b8b103992a6
unknown <ajameslusk@gmail.com>
2018-04-09 12:52:23 -0400

    added most of needed functions to store.js

--- scripts/store.js
+++ scripts/store.js
@@ -2,6 +2,7 @@
 /* global shoppingList, cuid, Item */
 //eslint-disable-next-line no-unused-vars
 const store = (function (){
+  
   const items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
@@ -10,7 +11,39 @@ const store = (function (){
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
+
+  function findById(id){
+    return items.find(item => item.id === id);
+  }
+
+  function addItem(name){
+    try{
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    }catch(e){
+      console.log(`Cannot add Item: ${e.message}`);
+    }
+  }
+
+  function findAndToggleChecked(id){
+    this.findById(id).checked =  !this.findById(id).checked;
+  }
+
+  function findAndUpdateName(id, newName){
+    try{
+      Item.validateName(newName);
+      Item.findById(id).name = newName;
+    }catch(e){
+      console.log(`Cannot update Item name: ${e.message}`);
+    }
+  }
+
+  function findAndDelete(id){
+    items.splice(items.findIndex(Item.findById(id)), 1);
+  }
+
   return {
-    items, hideCheckedItems, searchTerm
+    items, hideCheckedItems, searchTerm, findById, 
+    addItem, findAndToggleChecked, findAndUpdateName, findAndDelete,
   };
 }());
