7f7658d0cb9bd852ca4b28e63fac755f2d4cdddc
unknown <ajameslusk@gmail.com>
2018-04-09 12:24:47 -0400

    addItemToShoppingList now uses Item module

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global store, cuid */
+/* global store, Item */
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -57,7 +57,13 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try{
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    } catch(e){
+      console.log(`Cannot add item ${e.message}`);
+    }
   }
   
   function handleNewItemSubmit() {
